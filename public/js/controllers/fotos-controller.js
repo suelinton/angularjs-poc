d4f931f59dc3380f403fb6160de5a955b6a48773
Suélinton Farias <suelinton.farias@gmail.com>
2019-05-20 17:55:32 -0300

    aula 5.

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,9 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
 	
 	$scope.fotos = []; 
-
+	
+	$scope.filtro = '';
+	
 	$http.get('/v1/fotos')
 	.success(retorno=>{
 		console.log(retorno);
