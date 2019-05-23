b8763d0ccbd7e4a40e7b284a7cdf10252cd9c059
Suélinton Farias <suelinton.farias@gmail.com>
2019-05-23 18:12:03 -0300

    aula 6

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,17 +1,32 @@
 angular.module('minhasDiretivas', [])
-    .directive('meuPainel', function() {
+	.directive('meuPainel', function() {
+
+		var ddo = {};
+
+		ddo.restrict = "AE";
+        ddo.transclude = true;
+
+
+		ddo.scope = {
+            titulo: '@'
+        };
+
+        ddo.templateUrl = 'js/directives/meu-painel.html';
+
+		return ddo;
+	})
+    .directive('minhaFoto', function() {
 
         var ddo = {};
 
         ddo.restrict = "AE";
 
         ddo.scope = {
-            titulo: '@'
+            titulo: '@',
+            url: '@'
         };
 
-        ddo.transclude = true;
-
-        ddo.templateUrl = 'js/directives/minhas-diretivas.html';
-
+        ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';           
+        
         return ddo;
     });
