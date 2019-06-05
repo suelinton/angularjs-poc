bc8171db993123c1bf53e5bf09ed52a698587c6a
Suélinton Farias <suelinton.farias@gmail.com>
2019-06-05 18:52:58 -0300

    Implementação da aula 8.

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,22 +1,46 @@
 angular.module('alurapic')
-    .controller('FotoController', function ($scope, $http) {
+    .controller('FotoController', function ($scope, $http, $routeParams) {
         $scope.foto = {};
         $scope.mensagem = '';
 
+        if ($routeParams.fotoId) {
+            $http.get('/v1/fotos/' + $routeParams.fotoId)
+                .success(function (foto) {
+                    $scope.foto = foto;
+                })
+                .error(function (erro) {
+                    console.log(erro);
+                    $scope.mensagem = 'Não foi possível obter a foto'
+                });
+        }
+
         $scope.submeter = function () {
             if ($scope.formulario.$valid) {
-                console.log($scope.formulario)
-                $http.post('/v1/fotos', $scope.foto)
-                    .success(function () { 
-                        $scope.foto = {};
+                if ($routeParams.fotoId) {
 
-                        $scope.formulario.$dirty = false;
-                        $scope.mensagem = 'Foto add com sucesso' 
-                    })
-                    .error(function (erro) { 
-                        console.log('Não foi possível cadastrar a foto'); 
-                        $scope.mensagem = 'Não foi possível cadastrar a foto'; 
-                    })
+                    $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
+                        .success(function () {
+                            $scope.mensagem = 'Foto '+$scope.foto.titulo +' alterada com sucesso';
+
+                        })
+                        .error(function (erro) {
+                            console.log(erro);
+                            $scope.mensagem = 'Não foi possível alterar';
+                        });
+
+                } else {
+                    $http.post('/v1/fotos', $scope.foto)
+                        .success(function () {
+                            $scope.foto = {};
+
+                            $scope.formulario.$dirty = false;
+                            $scope.mensagem = 'Foto add com sucesso'
+                        })
+                        .error(function (erro) {
+                            console.log('Não foi possível cadastrar a foto');
+                            $scope.mensagem = 'Não foi possível cadastrar a foto';
+                        })
+                }
             }
         };
     });
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -4,6 +4,8 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 	
 	$scope.filtro = '';
 	
+	$scope.mensagem = '';
+
 	$http.get('/v1/fotos')
 	.success(retorno=>{
 		console.log(retorno);
@@ -12,5 +14,18 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 	.error(erro=>{
 		console.log(erro);
 	});
+
+	$scope.remover = function(foto) {
+        $http.delete('/v1/fotos/' + foto._id)
+        .success(function() {
+			var indiceDaFoto = $scope.fotos.indexOf(foto);
+            $scope.fotos.splice(indiceDaFoto, 1);
+            $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
+        })
+        .error(function(erro) {
+			console.log(erro);
+			$scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
+		});
+    };
 
 });
